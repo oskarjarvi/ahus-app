f38e49ccc627ea3903d23af6d36a5d9caab450f1
“Kajsa <kajsataipale@gmail.com>
2018-05-15 15:09:27 +0200

    changed in the routing,some elements in the navbar is just static

--- src/components/Sidebar/Navigation.js
+++ src/components/Sidebar/Navigation.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import '../../App.css';
 const Navigation = () => (
   <ul>
+    <Link to="/"><li>På gång i huset</li></Link>
     <Link to="/ErrorReport"><li>Felanmälan</li></Link>
     <Link to="/EventPage"><li>Eventsida</li></Link>
-    <Link to="/MapPage"><li>Karta</li></Link>
+    <Link to="/"><li>Karta</li></Link>
     <Link to="/MyConnections"><li>Mina Kontaker</li></Link>
+    <Link to="/"><li>+</li></Link>
   </ul>
 )
 
