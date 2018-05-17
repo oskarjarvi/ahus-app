8519fb40090fb327f168d174be9dd75355ffe07c
“Kajsa <kajsataipale@gmail.com>
2018-05-17 20:15:27 +0200

    continued  with the errorreport form

--- src/components/Pages/ErrorReport.js
+++ src/components/Pages/ErrorReport.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import {CSSTransitionGroup} from "react-transition-group";
 import { Link } from 'react-router-dom';
 import '../../App.css';
+import base from '../base';
 
-const ErrorReport = () =>(
+class ErrorReport extends Component {
+  constructor(props) {
+    super(props);
+    this.errorReport = this.errorReport.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+      address: '',
+      roomnumber: '',
+      message: '',
+      email: '',
+      phone: ''
+    }
+
+  }
+
+  errorReport(e){
+    e.preventDefault();
+    // base.database().ref('error-report').push(this.state.name, this.state.address, this.state.roomnumber, this.state.message, this.state.email, this.state.phone)
+  }
+
+  handleChange(e){
+    this.setState({ [e.target.name]: e.target.value });
+  }
+    render() {
+      return(
   <CSSTransitionGroup
     transitionName="worksTransition"
     transitionAppear={true}
@@ -15,43 +42,45 @@ const ErrorReport = () =>(
         <form>
           <div>
             <label for="name">Namn*</label>
-            <input type="text" placeholder="namn" name="namn" required />
+            <input value={this.state.name} onChange={this.handleChange} type="text" placeholder="namn" name="namn" required />
           </div>
 
           <div>
-            <label for="adress">Adress*</label>
-            <input type="text" placeholder="adress" name="adress" required />
+            <label for="address">Adress*</label>
+            <input value={this.state.address} onChange={this.handleChange} type="text" placeholder="adress" name="address" required />
           </div>
 
           <div>
             <label for="roomnumber">Rum/rumsnr*</label>
-            <input type="text" name="roomnumber" required />
+            <input value={this.state.roomnumber} onChange={this.handleChange} type="text" name="roomnumber" required />
           </div>
 
           <div>
-            <label for="name">Beskrivning(max 3000 tecken)*</label>
-            <textarea></textarea>
+            <label for="message">Beskrivning(max 3000 tecken)*</label>
+            <textarea value={this.state.message} onChange={this.handleChange}></textarea>
           </div>
 
           <div>
             <label for="email">Mail</label>
-            <input type="text" name="email" required />
+            <input value={this.state.email} onChange={this.handleChange} type="text" name="email" required />
           </div>
 
           <div>
             <label for="phone">Telefon</label>
-            <input type="text" name="phone" required />
+            <input value={this.state.phone} onChange={this.handleChange} type="text" name="phone" required />
           </div>
 
     <p>Eran information kommer endast användas i syfte för att åtgärda det
      problem som anmälts. Efter genomfört arbete raderas er information
      från våran databas.</p>
-   <button type="submit" class="signupbtn">Skicka in</button>
+   <button type="submit" onClick={this.errorReport}>Skicka in</button>
     </form>
 <Link to="/"><li>tillbaka</li></Link>
      </div>
   </CSSTransitionGroup>
-  )
+    )
+  }
+}
 
 
 export default ErrorReport;
