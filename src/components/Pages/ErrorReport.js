9f6fd0bc46636a14eda12d2fd18f2a2a8dd0d3ec
“Kajsa <kajsataipale@gmail.com>
2018-05-17 16:54:45 +0200

    started with the errorreport form

--- src/components/Pages/ErrorReport.js
+++ src/components/Pages/ErrorReport.js
@@ -10,10 +10,45 @@ const ErrorReport = () =>(
     transitionAppearTimeout={500}
     transitionEnter={false}
     transitionLeave={false}>
-    <div className="page">
-      <Link to="/"><li>tillbaka</li></Link>
+    <div className="page error">
       <h1>Felanmälan</h1>
-      <p>här gör du Felanmälan!</p>
+        <form>
+          <div>
+            <label for="name">Namn*</label>
+            <input type="text" placeholder="namn" name="namn" required />
+          </div>
+
+          <div>
+            <label for="adress">Adress*</label>
+            <input type="text" placeholder="adress" name="adress" required />
+          </div>
+
+          <div>
+            <label for="roomnumber">Rum/rumsnr*</label>
+            <input type="text" name="roomnumber" required />
+          </div>
+
+          <div>
+            <label for="name">Beskrivning(max 3000 tecken)*</label>
+            <textarea></textarea>
+          </div>
+
+          <div>
+            <label for="email">Mail</label>
+            <input type="text" name="email" required />
+          </div>
+
+          <div>
+            <label for="phone">Telefon</label>
+            <input type="text" name="phone" required />
+          </div>
+
+    <p>Eran information kommer endast användas i syfte för att åtgärda det
+     problem som anmälts. Efter genomfört arbete raderas er information
+     från våran databas.</p>
+   <button type="submit" class="signupbtn">Skicka in</button>
+    </form>
+<Link to="/"><li>tillbaka</li></Link>
      </div>
   </CSSTransitionGroup>
   )
