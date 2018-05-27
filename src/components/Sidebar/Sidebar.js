2ac03b462a8373eda5172b71ce3b75a123540b76
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-05-27 14:07:45 +0200

    trying to fix logged in view

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,15 +4,31 @@ import ErrorReport from '../Pages/Error/ErrorReport';
 import EventPage from '../Pages/Event/EventPage';
 import MyConnections from '../Pages/Connections/MyConnections';
 import NewPage from '../Pages/NewPage/NewPage';
+import base from './../base';
 
 class Sidebar extends Component {
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      user : base.auth().currentUser,
+    }
+  }
+
+
 render(){
   return(
     <Switch>
       <Route exact path="/ErrorReport" component={ErrorReport}/>
+//If user is not logged in show these
       <Route exact path="/EventPage" component={EventPage}/>
       <Route exact path="/MyConnections" component={MyConnections}/>
+//if user is logged in show these
+<Route exact path="/EventPage" component={EventPage}/>
+<Route exact path="/MyConnections" component={MyConnections}/>
+
       <Route exact path="/NewPage" component={NewPage}/>
+
     </Switch>
   )
 
