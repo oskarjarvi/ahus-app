ac927d8e48467b32557d36f804b3f32ebe4e9343
“Kajsa <kajsataipale@gmail.com>
2018-05-29 11:26:59 +0200

    fixed the transition on the menu when your on another page than home

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -39,7 +39,7 @@ class Navbar extends Component {
 
             <Main></Main>
             <HamburgerNavigation></HamburgerNavigation>
-        </ul> ) : ""}
+        </ul> ) : <ul className="staticMenu Closed"> </ul>}
 
 
 
